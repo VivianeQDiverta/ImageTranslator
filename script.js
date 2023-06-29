abd9dc05246159ceffb73a933d94f54949981f49
Viviane Qian <viviane@diverta.co.jp>
2023-06-29 13:32:26 +0900

    send binary image directly to worker to test it

---
+++ script.js
@@ -0,0 +1,21 @@
+const imageFile = document.querySelector('#image-file');
+const translateBtn = document.querySelector('#translate-btn');
+
+translateBtn.addEventListener('click', () => {
+  if (imageFile.files.length === 0) {
+    alert('Please select an image');
+    return;
+  }
+  const file = imageFile.files[0];
+  const reader = new FileReader();
+  reader.readAsBinaryString(file);
+  reader.onload = () => {
+    fetch(`https://image-translator-worker.viviane.workers.dev/gcp/detect-text`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ binaryImage: btoa(reader.result) }),
+    }).then((res) => res.json());
+  };
+});
