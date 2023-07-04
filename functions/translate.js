af7ff745d4923d0872ddc0b43b33a9d089ecb2f4
Viviane Qian <viviane@diverta.co.jp>
2023-07-04 14:42:49 +0900

    add cors headers

--- functions/translate.js
+++ functions/translate.js
@@ -4,6 +4,19 @@ export async function onRequestPost(context) {
   return new Response(html, {
     headers: {
       'Content-Type': 'text/html',
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
+}
+
+export async function onRequestOptions(context) {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': 'POST, OPTIONS',
+      'Access-Control-Max-Age': '86400',
     },
   });
 }
