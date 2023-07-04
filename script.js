f1a274b09911439aa8d5e0c2f1996b3a32e41daf
Viviane Qian <viviane@diverta.co.jp>
2023-07-04 11:53:00 +0900

    await async response instead of chaining promises

--- script.js
+++ script.js
@@ -3,38 +3,45 @@ const translateBtn = document.querySelector('#translate-btn');
 const targetLang = document.querySelector('#target-lang');
 const translateBtnSpinner = document.querySelector('#translate-btn-spinner');
 
-translateBtn.addEventListener('click', (e) => {
+translateBtn.addEventListener('click', async (e) => {
   e.preventDefault();
   if (imageFile.files.length === 0) {
     alert('Please select an image');
     return;
   }
+  // disable all inputs
+  translateBtnSpinner.classList.remove('d-none');
+  imageFile.disabled = true;
+  targetLang.disabled = true;
+  translateBtn.disabled = true;
+
+  // read file
   const file = imageFile.files[0];
   const reader = new FileReader();
   reader.readAsBinaryString(file);
-  reader.onload = () => {
-    // disable all inputs
-    translateBtnSpinner.classList.remove('d-none');
-    imageFile.disabled = true;
-    targetLang.disabled = true;
-    translateBtn.disabled = true;
-    // send request
-    fetch(`/translate`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'text/html',
-      },
-      body: JSON.stringify({
-        binaryImage: btoa(reader.result),
-        targetLang: targetLang.value,
-      }),
-    })
-      .then((res) => res.text())
-      .then((text) => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(text, 'text/html');
-        document.querySelector('body').innerHTML =
-          doc.querySelector('body').innerHTML;
-      });
-  };
+  const binaryImage = await new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.readAsBinaryString(file);
+    reader.onloadend = () => {
+      resolve(reader.result);
+    };
+  });
+
+  // send request
+  const res = await fetch(`/translate`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: JSON.stringify({
+      binaryImage: btoa(binaryImage),
+      targetLang: targetLang.value,
+    }),
+  })
+
+  // parse response
+  const text = await res.text();
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(text, 'text/html');
+  document.querySelector('body').innerHTML = doc.querySelector('body').innerHTML;
 });
