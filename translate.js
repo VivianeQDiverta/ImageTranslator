d467e5ec96fcce55de47e28592ccc4e95add585b
Viviane Qian <viviane@diverta.co.jp>
2023-07-06 14:42:17 +0900

    show/hide annotations individually

--- translate.js
+++ translate.js
@@ -1,29 +1,37 @@
-// identify an element to observe
+// observe for body changes
 const body = document.querySelector('body');
 
 // observe body for childList changes and add listeners if #result is added
 const observer = new MutationObserver(() => {
-  if (document.getElementById('result') === null) return;
+  const annotationsContainer = document.getElementById('annotationsContainer');
+  if (document.getElementById('result') === null || !annotationsContainer)
+    return;
+
   const switchContainer = document.getElementById('switch-container');
   const showAnnotationsSwitch = document.getElementById('show-annotations');
-  const annotationsContainer = document.getElementById('annotationsContainer');
 
-  // Hide switch if no annotations
-  if (
-    document.getElementById('annotationsContainer') === null &&
-    switchContainer
-  ) {
-    switchContainer.style.display = 'none';
-  }
+  // Display switch container
+  switchContainer.style.display = 'block';
 
-  // Show or hide annotations
+  // Show or hide annotations depending on switch state
   showAnnotationsSwitch.addEventListener('change', (event) => {
-    if (event.target.checked) {
-      annotationsContainer.classList.remove('hidden');
-    } else {
-      annotationsContainer.classList.add('hidden');
-    }
+    toggleAnnotations(annotationsContainer, event.target.checked);
+  });
+
+  // add click handler to show/hide each annotation individually
+  annotationsContainer.childNodes.forEach((annotation) => {
+    annotation.addEventListener('click', () =>
+      annotationClickHandler(annotation)
+    );
   });
 });
 
 observer.observe(elementToObserve, { childList: true });
+
+const toggleAnnotations = async (annotationsContainer, checked) => {
+  annotationsContainer.style.display = checked ? 'block' : 'none';
+};
+
+const annotationClickHandler = (annotation) => {
+  annotation.style.opacity = annotation.style.opacity === '0' ? '1' : '0';
+};
