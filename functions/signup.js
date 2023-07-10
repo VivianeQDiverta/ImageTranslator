01c799fba0e0d3114d4db267723708cba8f398f3
Viviane Qian <viviane@diverta.co.jp>
2023-07-10 18:04:10 +0900

    hash password

--- functions/signup.js
+++ functions/signup.js
@@ -1,11 +1,14 @@
+import bcrypt from './libs/bcrypt.min.js';
+
 export async function onRequestPost(context) {
   const db = context.env.DB;
   const body = await context.request.json();
   const { username, password } = body;
+  const hash = bcrypt.hashSync(password, 10);
   try {
     await db
       .prepare('INSERT INTO users (username, password) VALUES (?, ?)')
-      .bind(username, password)
+      .bind(username, hash)
       .run();
     return new Response(JSON.stringify({ success: true }), {
       headers: {
