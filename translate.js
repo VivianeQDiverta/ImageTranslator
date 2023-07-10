5206ed31a8dbf97a9361a26d1582c0fbe4812461
Viviane Qian <viviane@diverta.co.jp>
2023-07-10 15:27:15 +0900

    compute result div size w/ annotations  before download

--- translate.js
+++ translate.js
@@ -35,14 +35,37 @@ const annotationClickHandler = (annotation) => {
   annotation.style.opacity = annotation.style.opacity === '0' ? '1' : '0';
 };
 
+const computeResultSize = (minWidth, minHeight) => {
+  const annotations = document.querySelector(
+    '.annotationsContainer'
+  ).childNodes;
+  const { width, height } = Array.from(annotations).reduce(
+    (acc, annotation) => {
+      const top = parseInt(annotation.style.top.replace('px', ''));
+      const left = parseInt(annotation.style.left.replace('px', ''));
+      const bottom = top + annotation.offsetHeight;
+      const right = left + annotation.offsetWidth;
+      return {
+        width: right > acc.width ? right : acc.width,
+        height: bottom > acc.height ? bottom : acc.height,
+      };
+    },
+    { width: minWidth, height: minHeight }
+  );
+  return { width, height };
+};
+
 const downloadClickHandler = () => {
   const resultDiv = document.getElementById('result');
+  // adjust result div size to fit all annotations
+  const { width, height } = computeResultSize(resultDiv.offsetWidth, resultDiv.offsetHeight);
+  resultDiv.style.width = `${width}px`;
+  resultDiv.style.height = `${height}px`;
   // convert result div to blob and download it using html-to-image
   htmlToImage.toBlob(resultDiv).then(function (blob) {
     var file = new File([blob], 'result.jpg', {
       type: 'application/octet-stream',
     });
     window.location = URL.createObjectURL(file);
-    URL.revokeObjectURL(url) // free memory
   });
 };
