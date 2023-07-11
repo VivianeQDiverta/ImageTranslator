4b82755e0bc8b69f54b20ae0f351b481c2c1635a
Viviane Qian <viviane@diverta.co.jp>
2023-07-11 12:08:28 +0900

    fix sign and add verify

--- functions/libs/jwt.js
+++ functions/libs/jwt.js
@@ -1,6 +1,8 @@
 const sign = async (data, secret) => {
   const encoder = new TextEncoder();
-  const dataBuffer = encoder.encode(data);
+  const header = btoa(JSON.stringify({ alg: 'HS256', typ: 'JWT' }));
+  const payload = btoa(JSON.stringify({ data }));
+  const dataBuffer = encoder.encode(`${header}.${payload}`);
   const secretBuffer = encoder.encode(secret);
   const key = await crypto.subtle.importKey(
     'raw',
@@ -9,13 +11,43 @@ const sign = async (data, secret) => {
     false,
     ['sign']
   );
-  const signature = await crypto.subtle.sign('HMAC', key, dataBuffer);
-  const base64sign = btoa(String.fromCharCode(...new Uint8Array(signature)));
-  const header = btoa(JSON.stringify({ alg: 'HS256', typ: 'JWT' }));
-  const payload = btoa(JSON.stringify({ data }));
-  return `${header}.${payload}.${base64sign}`; 
+  const signatureBuffer = await crypto.subtle.sign('HMAC', key, dataBuffer);
+  const signature = btoa(
+    String.fromCharCode(...new Uint8Array(signatureBuffer))
+  );
+  return `${header}.${payload}.${signature}`;
+};
+
+const verify = async (token, secret) => {
+  const [header, payload, signature] = token.split('.');
+  const encoder = new TextEncoder();
+  const dataBuffer = encoder.encode(`${header}.${payload}`);
+  const secretBuffer = encoder.encode(secret);
+  const key = await crypto.subtle.importKey(
+    'raw',
+    secretBuffer,
+    { name: 'HMAC', hash: 'SHA-256' },
+    false,
+    ['verify']
+  );
+  const signatureBuffer = Uint8Array.from(atob(signature), (c) =>
+    c.charCodeAt(0)
+  );
+  const verified = await crypto.subtle.verify(
+    'HMAC',
+    key,
+    signatureBuffer,
+    dataBuffer
+  );
+
+  if (!verified) {
+    throw new Error('Invalid signature');
+  } else {
+    return JSON.parse(atob(payload)).data;
+  }
 };
 
 export default {
   sign,
+  verify,
 };
