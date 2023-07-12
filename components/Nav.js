5d1e58dc532e68cd7c7b4d476754c0f779d92a4b
Viviane Qian <viviane@diverta.co.jp>
2023-07-12 11:02:44 +0900

    change nav for navbar

--- components/Nav.js
+++ components/Nav.js
@@ -5,10 +5,19 @@ class DefaultNav extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-      <nav class="nav justify-content-center">
-        <a id="home" class="nav-link" href="/">Image Translator</a>
-        <a id="signup" class="nav-link" href="/signup">Sign up</a>
-        <a id="signin" class="nav-link" href="/signin">Sign in</a>
+      <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+        <div class="container-fluid">
+          <a id="home" class="navbar-brand" href="/">Image Translator</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="navbar-nav">
+              <a id="signup" class="nav-item nav-link" href="/signup">Sign up</a>
+              <a id="signin" class="nav-item nav-link" href="/signin">Sign in</a>
+            </div>
+          </div>
+        </div>
       </nav>
     `;
   }
@@ -21,10 +30,19 @@ class SignedInNav extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-      <nav class="nav justify-content-center">
-        <a id="home" class="nav-link" href="/">Image Translator</a>
-        <a id="history" class="nav-link" href="/history">History</a>
-        <a id="signout" class="nav-link" href="/">Sign out</a>
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+        <div class="container-fluid">
+          <a id="home" class="navbar-brand" href="/">Image Translator</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="navbar-nav">
+              <a id="history" class="nav-link" href="/history">History</a>
+              <a id="signout" class="nav-link" href="/">Sign out</a>
+            </div>
+          </div>
+        </div>
       </nav>
     `;
   }
@@ -34,9 +52,17 @@ const token = localStorage.getItem('token');
 if (token) {
   customElements.define('nav-component', SignedInNav);
   document.getElementById('signout').addEventListener('click', () => {
-    console.log('signout');
     localStorage.removeItem('token');
   });
 } else {
   customElements.define('nav-component', DefaultNav);
 }
+
+// set active nav link
+document.addEventListener('DOMContentLoaded', () => {
+  const path = window.location.pathname.slice(1, -1);
+  const navLinks = document.getElementById(path);
+  if (navLinks) {
+    navLinks.classList.add('active');
+  }
+});
